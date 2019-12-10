6eb72f0dd00037b0042999648a6be70acfc9cfa9
oxibex <oxibex@gmail.com>
2019-12-10 12:13:30 +0300

    microservices - eureka server

---
+++ microservice/eureka-server/src/main/java/microservice/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Nikolay Chechenko  Nikolay.Chechenko@connectia.com
+ * on 10.12.2019
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
